5a9780b6227f790c03185eaec333de6d33a3e6d1
Luca Clementi <luca.clementi@gmail.com>
2013-08-02 09:11:55 -0700

    pragma: adding utils module

---
+++ pragma/utils.py
@@ -0,0 +1,51 @@
+
+from subprocess import PIPE, Popen
+import subprocess
+import os
+import shlex
+
+
+def getOutputAsList(cmdline, inputString=None):
+    """ run popen pipe inputString and return a touple of
+    (the stdout as a list of string, return value of the command)
+    """
+    if type(cmdline) is str:
+        # needs to make a list
+        cmdline = shlex.split(cmdline)
+    p = Popen(cmdline, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    grep_stdout = p.communicate(input=inputString)[0]
+    p.wait()
+    return (grep_stdout.split('\n'), p.returncode)
+
+
+
+# copied from stackoverflow
+# http://stackoverflow.com/questions/377017/test-if-executable-exists-in-python/377028
+def which(program):
+
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+    return None
+
+
+def any(iterable):
+    for element in iterable:
+        if element:
+            return True
+    return False
+
+def all(iterable):
+    for element in iterable:
+        if not element:
+            return False
+    return True
