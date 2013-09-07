8177b45070697f02e3d9fdc50cf8b5a970891efd
Pongsakorn U-chupala <knightbaron@gmail.com>
2013-09-08 02:52:25 +0700

    Implement gz extraction (simple)

--- drivers/opennebula_kvm/opennebula/settings.rb
+++ drivers/opennebula_kvm/opennebula/settings.rb
@@ -8,3 +8,4 @@ LOG_FILE = "/tmp/opennebula_kvm.log"
 DATASTORE_ID = 1
 # TODO: Think of a better way to assign FQDN dynamically
 FQDN = "foobar"
+TEMP_PATH = "/tmp"
--- drivers/opennebula_kvm/opennebula/utils.rb
+++ drivers/opennebula_kvm/opennebula/utils.rb
@@ -1,3 +1,11 @@
 def inject_context_script(file_path)
     # TODO: Write me
 end
+
+def decompress(file, temp_path)
+    # TODO: We need a more clever version of this
+    filename = File.basename(file, ".*")
+    target = File.join(temp_path, filename)
+    system("gunzip -c #{file} > #{target}")
+    return target
+end
