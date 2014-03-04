7c847f8b071a12827a5e74964122cc1a50bfb838
Pongsakorn U-chupala <knightbaron@gmail.com>
2014-03-04 11:06:21 -0800

    Various changes
    - Update CloudFront skeleton
    - Update use "repodir" instead of "base_dir"

--- repository/cloudfront/__init__.py
+++ repository/cloudfront/__init__.py
@@ -5,3 +5,11 @@ class CloudFront(BaseRepository):
     """CloudFront Repository Class"""
     def __init__(self, settings):
         super(CloudFront, self).__init__(settings)
+
+    # TODO: Write me
+    def get_vcdb(self):
+        pass
+
+    # TODO: Write me
+    def download_vc(self, vc_name):
+        pass
