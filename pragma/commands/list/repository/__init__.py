9a31002cd6a55423807a5ed3f67fb5a72bbb39ab
shavasmallen <ssmallen@sdsc.edu>
2015-10-27 11:18:41 -0700

    Forgot to check in new list command

---
+++ pragma/commands/list/repository/__init__.py
@@ -0,0 +1,29 @@
+import os
+import pragma.commands
+import pragma.repository.utils
+
+class Command(pragma.commands.list.command):
+
+        """
+        List available virtual cluster images in repository
+
+        <param type='string' name='basepath'>
+        The absolute path of pragma_boot
+        </param>
+
+        <example cmd='list repository'>
+        List available virtual clusters images
+        </example>
+        """
+        def run(self, params, args):
+		repository = pragma.utils.getRepository()
+		vc_db_filepath = repository.get_vcdb_file()
+
+	        print "NAME"
+		with open(vc_db_filepath, 'r') as vc_db:
+			for line in vc_db:
+				(vcname, xmlpath) = line.split(',')
+                		print vcname
+
+
+RollName = "pragma_boot"
