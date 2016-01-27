b1ed49bbeb46cb686db9e9f498f74ce0ebf06990
root <root@fiji.rocksclusters.org>
2016-01-27 01:15:49 -0800

    Checking in functions for driver

--- pragma/drivers/__init__.py
+++ pragma/drivers/__init__.py
@@ -8,6 +8,12 @@ class Driver:
 
 	@staticmethod
 	def factory(driver_name):
+		"""
+		Create an instance of driver 
+
+		:param driver_name: Name of pragma driver
+		:return: An instance of pragma driver
+		"""
 		driver_class = 'pragma.drivers.%s.Driver' % driver_name
 		logger.info( "Loading driver %s" % driver_class )
 		fullpath = driver_class.split(".")
@@ -18,8 +24,45 @@ class Driver:
 		return klass()
 
 	def allocate(self, cpus, memory, key, vc_in, vc_out, repository):
-		pass
+		"""
+		Allocate a new virtual cluster from Rocks
+
+		:param cpus: Number of CPUs to instantiate
+		:param memory: Amount of memory per compute node
+		:param key: Path to SSH Key to install
+		:param vc_in: Path to virtual cluster specification
+		:param vc_out: Path to new virtual cluster information
+		:param repository: Path to virtual cluster repository
+		:return:
+		"""
+		raise NotImplementedError("Please implement allocate method")
+
+        def clean(self, vcname):
+                """
+                Unallocate virtual cluster and clean up disks.
+
+                :param vcname: Name of virtual cluster to be cleaned
+                :return: True if clean was successful, otherwise False
+                """
+		raise NotImplementedError("Please implement clean method")
 
         def deploy(self, vc_in, vc_out, temp_dir):
-		pass
+		"""
+		Deploy the specified virtual cluster
 
+		:param vc_in: Virtual cluster source specification
+		:param vc_out: Network configuration for new virtual cluster
+		:param temp_dir: Path to temporary directory
+		:return:
+		"""
+		raise NotImplementedError("Please implement deploy method")
+
+	def shutdown(self, vcname):
+		"""
+		Shutdown the nodes of the specified virtual cluster.
+
+		:param vcname: Name of running virtual cluster
+
+		:return: True if cluster is shutdown, otherwise False
+		"""
+		raise NotImplementedError("Please implement shutdown method")
