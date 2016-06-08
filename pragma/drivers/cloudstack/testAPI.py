605dd34ce37a13ccea187e812158e75057826370
nadyawilliams <nadya@sdsc.edu>
2016-06-08 16:14:55 -0700

    move keys to separate file

--- pragma/drivers/cloudstack/testAPI.py
+++ pragma/drivers/cloudstack/testAPI.py
@@ -1,23 +1,73 @@
 #!/usr/bin/env python
 
+# user  apikey and secret key are in the file mykeys.py
+# do not add mykeys.py to git repo
+from mykeys import *
 from cloudstack import CloudStackCall
 
 ### Main ###
-
-apikey    = 'nWcPrqXC60UAfHyRqXsqm-JZPTHiCIQmMGO0eSp5_GyO9-0p51qC05a7xpgvtVAC1CM-'
-secretkey = 'Y5kSgRBn70NpGRSlmeL9ea6lkZj1fn77VRZKZxz0GkXjrwl86fW72mY5OxE2SAlqX3sudIVe'
-baseurl   ='http://163.220.56.65:8080/client/api?'
+# base settings for the cloudstack configuration
+baseurl   = 'http://163.220.56.65:8080/client/api?'
 templatefilter = 'community'
 networkoffering = 'DefaultIsolatedNetworkOfferingWithSourceNatService'
 
 apicall = CloudStackCall(baseurl, apikey, secretkey, templatefilter)
-#apicall.allocateVirtualCluster("biolinux-frontend-original",1,"biolinux-compute-original",1,2)
-apicall.listNetworkOfferings(networkoffering)
-apicall.listNetworks()
 
-# tested ok
-apicall.listTemplates('biolinux-frontend-original')
-#apicall.listVirtualMachines()
+def ListNetworks ():
+    response = apicall.listNetworks()
+    print response.keys()
+    count = response['count']
+    for i in range(count):
+        d = response['network'][i]
+        print "network:", d['name'], d['id'], d['networkofferingid'], d['cidr'], d['netmask']
+
+def ListTemplates (name = None):
+    response = apicall.listTemplates(name)
+    count = response['count']
+    for i in range(count):
+        d = response['template'][i]
+        print "template:", d['name'], d['id']
+
+def ListNetworkOfferings(name = None):
+    response = apicall.listNetworkOfferings(name)
+    # in response  count does not always corresponds to the list length, dont use count = response['count']
+    count = len(response['networkoffering'])
+    for i in range(count):
+        d = response['networkoffering'][i]
+        print "networkOffering:", d['name'], d['id']
+
+def ListVMs (name = None, id = None):
+    response = apicall.listVirtualMachines(name)
+    count = response['count']
+    for i in range(count):
+        d = response['virtualmachine'][i]
+        # deal with multiple nics
+        numnics =  len(d['nic'])
+        for n in range(numnics):
+            nic = d['nic'][n]
+            print "VMs:", d["name"], nic['ipaddress'], nic['networkname']
+
+def GetIPs ():
+    ips = apicall.getVirtualMachineIPs()
+    print ips
+
+#apicall.allocateVirtualCluster("biolinux-frontend-original",1,"biolinux-compute-original",1,2)
+
 #apicall.listServiceOfferings()
 #apicall.allocateVirtualMachine(1, "biolinux-compute-original")
 
+############## tested ok ############## 
+#ListNetworks()
+
+#ListTemplates()
+#name = 'biolinux-frontend-original'
+#ListTemplates(name)
+
+#ListNetworkOfferings()
+#ListNetworkOfferings(networkoffering)
+
+#ListVMs()
+# when giving a name, the outputcontains all vms with the name substring
+#ListVMs(name = 'vm-48-compute-1')
+
+#GetIPs()
